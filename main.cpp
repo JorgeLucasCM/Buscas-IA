fa162145c5a1fdac84c04a98410218870829f0e8
JorgeLucasCM <jorgelcm.ec@gmail.com>
2021-02-26 13:36:36 -0300

    Teste com ponteiro
    Co-authored-by: Gregório Neto <GregorioNeto@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ public:
 	int indice;
 	string nome;
 	bool visitado = false;
-	vector<Vertice> adj;
+	vector<Vertice*> adj;
 	vector<int> adjDist;
 public:
 	Vertice(){
@@ -41,12 +41,12 @@ public:
 		cout << indice << " " << nome << " " << visitado << endl;
 	}
 
-	void addAdj(Vertice v1, int dist){
+	void addAdj(Vertice *v1, int dist){
 		this->adj.push_back(v1);
 		this->adjDist.push_back(dist);
 	}
 
-	Vertice getAdj(int pos){
+	Vertice* getAdj(int pos){
 		return this->adj[pos];
 	}
 
@@ -58,22 +58,22 @@ public:
 };
 
 class Grafo{
-	queue<Vertice> fila;
+	queue<Vertice*> fila;
 
 public:
 	Grafo(){
 
 	}
 
-	void bfs(Vertice inicial, Vertice final){
-		Vertice atual = inicial;
-		cout << "Iniciando da cidade " << inicial.getNome() << endl;
+	void bfs(Vertice *inicial, Vertice *final){
+		Vertice *atual = inicial;
+		cout << "Iniciando da cidade " << inicial->getNome() << endl;
 		while(true){
-			for(int i = 0 ; i < atual.adj.size(); i++){
-				if(!atual.getAdj(i).getVisitado()){
-					cout << "Vistando cidade " << atual.getAdj(i).getNome() << endl;
-					atual.getAdj(i).setVisitado();
-					fila.push(atual.getAdj(i));
+			for(int i = 0 ; i < atual->adj.size(); i++){
+				if(!atual->getAdj(i)->getVisitado()){
+					cout << "Vistando cidade " << atual->getAdj(i)->getNome() << endl;
+					atual->getAdj(i)->setVisitado();
+					fila.push(atual->getAdj(i));
 				}
 			}	
 			if(!fila.empty()){
@@ -92,37 +92,37 @@ public:
 
 int main(){
 	//Adicionando Vertices
-	Vertice Oradea(1,"Oradea");
-	Vertice Zerind(2,"Zerind");
-	Vertice Arad(3,"Arad");
-	Vertice Timisoara(4,"Timisoara");
-	Vertice Lugoj(5,"Lugoj");
-	Vertice Mehadia(6,"Mhadia");
-	Vertice Drobeta(7,"Drobeta");
-	Vertice Craiova(8,"Craiova");
-	Vertice Pitesti(9,"Pitesti");
-	Vertice RimnicuVilecea(10,"RimnicuVilecea");
-	Vertice Sibiu(11,"Sibiu");
-	Vertice Fagaras(12,"Fagaras");
-	Vertice Bucharest(13,"Bucharest");
-	Vertice Giurgiu(14,"Giurgiu");
-	Vertice Urziceni(15,"Urziceni");
-	Vertice Hirsova(16, "Hirsova");
-	Vertice Eforie(17, "Eforie");
-	Vertice Vaslui(18, "Vaslui");
-	Vertice Iasi(19, "Iasi");
-	Vertice Neamt(20, "Neamt");
+	Vertice *Oradea  = new Vertice(1,"Oradea");
+	Vertice *Zerind = new Vertice(2,"Zerind");
+	Vertice *Arad = new Vertice(3,"Arad");
+	Vertice *Timisoara = new Vertice(4,"Timisoara");
+	Vertice *Lugoj = new Vertice(5,"Lugoj");
+	Vertice *Mehadia = new Vertice(6,"Mhadia");
+	Vertice *Drobeta = new Vertice(7,"Drobeta");
+	Vertice *Craiova = new Vertice(8,"Craiova");
+	Vertice *Pitesti = new Vertice(9,"Pitesti");
+	Vertice *RimnicuVilecea = new Vertice(10,"RimnicuVilecea");
+	Vertice *Sibiu = new Vertice(11,"Sibiu");
+	Vertice *Fagaras = new Vertice(12,"Fagaras");
+	Vertice *Bucharest = new Vertice(13,"Bucharest");
+	Vertice *Giurgiu = new Vertice(14,"Giurgiu");
+	Vertice *Urziceni = new Vertice(15,"Urziceni");
+	Vertice *Hirsova = new Vertice(16, "Hirsova");
+	Vertice *Eforie = new Vertice(17, "Eforie");
+	Vertice *Vaslui = new Vertice(18, "Vaslui");
+	Vertice *Iasi = new Vertice(19, "Iasi");
+	Vertice *Neamt = new Vertice(20, "Neamt");
 
-	Zerind.addAdj(Arad,75);
-	Sibiu.addAdj(Fagaras,99);
-	Oradea.addAdj(Zerind,71);
-	Oradea.addAdj(Sibiu,151);
-	
+	Oradea->addAdj(Zerind,71);	
+	Oradea->addAdj(Sibiu,151);
+	Zerind->addAdj(Arad,75);
+	Sibiu->addAdj(Fagaras,99);
+
 	
 
 
 	Grafo grafo;
-	grafo.bfs(Oradea,Fagaras);
+	grafo.bfs(Oradea, Fagaras);
 
 
 
